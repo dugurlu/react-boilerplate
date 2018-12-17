b67cbaa1dc4101d0b2b65889c9c1f63782035397
Deniz Ugurlu <mail@denizugurlu.de>
2018-12-17 16:16:46 +0100

    check for accessibility issues with axe

---
+++ src/DefaultErrorBoundary.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class DefaultErrorBoundary extends React.Component {
+  state = {
+    isError: false
+  }
+
+  static getDerivedStateFromError() {
+    return { isError: true }
+  }
+
+  render() {
+    const { isError } = this.state
+    const { children } = this.props
+
+    return isError ? <div>Somethin went wrong!</div> : children
+  }
+}
+
+DefaultErrorBoundary.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default DefaultErrorBoundary
