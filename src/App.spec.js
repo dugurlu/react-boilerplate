2b71946a5af6adb8b31ae4b52c196d4344a9b74b
Deniz Ugurlu <mail@denizugurlu.de>
2018-12-17 07:32:59 +0100

    add prettier and format all files

--- src/App.spec.js
+++ src/App.spec.js
@@ -4,7 +4,7 @@ import { render } from 'react-testing-library'
 import App from './App'
 
 describe('App', () => {
-    it('Renders without error', () => {
-        render(<App/>)
-    })
+  it('Renders without error', () => {
+    render(<App />)
+  })
 })
